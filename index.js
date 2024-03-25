e690f0eac7057d2346779ac3992925260f662b36
Michelle <azariahthesecond@gmail.com>
2024-03-25 15:10:06 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,40 +1,19 @@
-// Code your solution in this file!
-
-const returnFirstTwoDrivers = function (drivers) {
-    
-  const firstTwoDrivers = drivers.slice(0,2)
-
-  console.log(firstTwoDrivers);
-
-  return firstTwoDrivers;
-}
-const returnLastTwoDrivers = function (drivers) {
-
-  const lastTwoDrivers = drivers.slice(2,4)
-
-  console.log(lastTwoDrivers);
-
-  return lastTwoDrivers;
+function receivesAFunction(cb) {
+  console.log(cb());
+  
 
 }
 
-let selectingDrivers = []
+function returnsANamedFunction(){
 
-selectingDrivers.push(returnFirstTwoDrivers);
-selectingDrivers.push(returnLastTwoDrivers);
+  return function beforeall() {
+      return "before all";
+  };
+}
 
-const createFareMultiplier = (number) => {
+function returnsAnAnonymousFunction(){
 
-  return function multiplier (fare) {
-
-      return fare * number
+  return function () {
+      return "before all";
   }
-}
-
-const fareDoubler = createFareMultiplier(2);
-
-const fareTripler = createFareMultiplier(3);
-
-const selectDifferentDrivers = (drivers,returnFirstTwoDrivers) => {
-  return returnFirstTwoDrivers(drivers);
 }
